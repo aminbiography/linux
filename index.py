81752b871c2efe703f239dc375b35c2e78c007ff
aminbiography <biographyamin@gmail.com>
2024-12-03 17:04:05 +0600

    first commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# Linux #
+
+# https://www.kali.org/ 
+
+# Download > https://www.kali.org/get-kali/#kali-platforms 
+
+# Live Boot > https://www.kali.org/get-kali/#kali-live
+
+# Kali 2024.3 (64-bit) > Download
+
+# rufus > https://rufus.ie/en/
+
+# https://www.youtube.com/watch?v=ozGmd1Xt_cU
+
