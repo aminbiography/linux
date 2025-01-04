c25f758c898f47453a042f16b4cb301ef46a84b1
idolwave <idolwaveglobal@gmail.com>
2025-01-04 21:11:36 +0600

    updated

--- beginners.py
+++
@@ -1,6 +0,0 @@
-# Open Terminal in Kali Linux
-# Keyboard Shortcut: Press Ctrl + Alt + T to open the terminal.
-# Through GUI:
-# Click on the Applications menu in the top-left corner.
-# Navigate to System Tools.
-# Select Terminal.
